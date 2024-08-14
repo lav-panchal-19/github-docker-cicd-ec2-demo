9e370740a660c3a9b9cf212bfe75ea9b0ad0db24
Lav Panchal <98877970+lav-panchal-19@users.noreply.github.com>
2024-08-14 14:08:19 +0530

    Add eslint configuration (#3)
    * Add eslint configuration
    * check linting
    * fix linting

--- index.js
+++ index.js
@@ -139,5 +139,6 @@ app.use((req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server is running on http://localhost:${PORT}`);
 });
